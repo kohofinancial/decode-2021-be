f1d9ee1460e91aec41179c6c99bec6fdee175147
Yan Matagne <yan@koho.ca>
2021-03-05 17:08:08 -0500

    delete it

--- db/campaigns.js
+++ db/campaigns.js
@@ -36,7 +36,6 @@ const addTransactionToCampaign = async(id, transaction) => {
 }
 
 const deleteCampaignById = async(campaignId) => {
-  console.log("YMA"+campaignId)
   return Campaign.findOneAndDelete({_id: campaignId})
 }
 module.exports = {
